49293916e88b4177fbbb02a5382505a3444fded4
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-15 22:45:32 +0300

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+Deletion of  hypermedia pagination
 """
+
 import csv
+import math
 from typing import List, Dict
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -14,35 +18,41 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                self.__dataset = list(reader)[1:]
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Get the hyper index
+            Get the hyper index
 
-        Args:
-            index: Current page
-            page_size: Total size of the page
+            Args:
+                index: Current page
+                page_size: Total size of the page
 
-        Return:
-            Hyper index
+            Return:
+                Hyper index
         """
         result_dataset = []
         index_data = self.indexed_dataset()
         keys_list = list(index_data.keys())
-
         assert index + page_size < len(keys_list)
         assert index < len(keys_list)
 
@@ -57,7 +67,7 @@ class Server:
             else:
                 result_dataset.append(index_data[i])
 
-        next_index = index + page_size
+        next_index: int = index + page_size
 
         if index in keys_list:
             next_index
