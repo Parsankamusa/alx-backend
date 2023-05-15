5ce24188ce23345bd82bb6f43ea1e6fea841b29b
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-15 22:27:09 +0300

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -7,7 +7,7 @@ from typing import List, Tuple, Dict
 
 class Server:
      """Server class to paginate a database of popular baby names.
-    """
+     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -21,7 +21,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-         """
+          """
             Get the page
 
             Args:
@@ -30,7 +30,7 @@ class Server:
 
             Return:
                 List of the pagination done
-        """
+                """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
