7a7fe39a1c0f80a73263ea744d67c9d3e105fda8
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-17 20:37:10 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,7 +2,8 @@
 ''' FIFO caching: Create a class FIFOCache that inherits from BaseCaching
                   and is a caching system
 '''
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -15,21 +16,30 @@ class FIFOCache(BaseCaching):
           __init__ - method that initializes class instance
           put - method that adds a key/value pair to cache
           get - method that retrieves a key/value pair from cache '''
+
     def __init__(self):
+        ''' Initialize class instance. '''
         super().__init__()
+        self.keys = []
 
     def put(self, key, item):
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard oldest entry in cache to accommodate new entry. '''
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Discard the first item put in cache (FIFO algorithm)
-                oldest_key = next(iter(self.cache_data))
-                del self.cache_data[oldest_key]
-                print(f"DISCARD: {oldest_key}\n")
-
             self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
+
 
